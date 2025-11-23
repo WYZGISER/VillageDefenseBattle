ba3e44bb743d255863e87affef8d4085eb8617c2
wyz <646189002@qq.com.noreply.gitee.com>
2025-11-23 21:00:57 +0800

    瓦片地图数据功能实现

--- TdGame/TdGame/tile.h
+++ TdGame/TdGame/tile.h
@@ -1,1 +1,23 @@
-#pragma once
+#ifndef _TILE_H
+#define _TILE_H
+
+#include<vector>
+
+#define SIZE_TILE 48
+struct Tile {
+	enum class Direaction
+	{
+		None = 0,
+		Up,
+		Down,
+		Left,
+		Right
+	};
+	int terrian = 0;
+	int decoration = -1;
+	int special_flag = -1;
+	Direaction direction = Direaction::None;
+};
+
+typedef std::vector<std::vector<Tile>> TileMap;
+#endif // !_TILE_H
